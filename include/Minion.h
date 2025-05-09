df5159ec4842e1f09e8b5cf1502eff32748705e4
Zaynn-lea <gely.leandre@gmail.com>
2025-05-09 11:14:24 +0200

    forgot a constructor

--- include/Minion.h
+++ include/Minion.h
@@ -20,14 +20,16 @@ namespace ImpossibleBattleBoss
 	{
 	public:
 		cMinion();
+		cMinion(olc::vi2d spawnPos, std::map<MinionState, std::vector<olc::Sprite>> * sprites);
 		~cMinion();
 
 	private:
-		
+		MinionState state;
+		std::map<MinionState, std::vector<olc::Sprite>> * sprites;
 
 	public:
 		olc::Sprite * getCurrentSprite();
 
-		void update(cPlayer p, float deltaTime);
+		void update(cPlayer p, std::vector<std::vecto<cEntity>> * map, float deltaTime);
 	};
 };
--- src/Minion.cpp
+++ src/Minion.cpp
@@ -8,11 +8,17 @@ using namespace ImpossibleBattleBoss;
 
 cMinion::cMinion() {}
 
+cMinion::cMinion(olc::vi2d spawnPos, std::map<MinionState, std::vector<olc::Sprite>> * sprites)
+{
+	pos		= spawnPos;
+	this->sprites	= sprites;
+}
+
 
 olc::Sprite cMinion::getCurrentSprite()	{  return sprites[state][currentSprite];  }	// TOOD : curretSprite ?
 
 
-void cMinion::update(cPlayer p, float deltaTime)
+void cMinion::update(cPlayer p, std::vector<std::vector<cEntity>> * map, float deltaTime)
 {
 	// TODO
 }
