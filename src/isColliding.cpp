0d89453d26bb3b6d5e489aca662270c7f8ef7017
Zaynn-lea <gely.leandre@gmail.com>
2025-05-12 13:15:46 +0200

    hitbox colliding first version

---
+++ src/isColliding.cpp
@@ -0,0 +1,20 @@
+
+
+bool isColliding(cEntity * other)
+{
+	bool vertAlign, horzAlign;
+
+	olc::vi2d selfStart; selfEnd, compStart, compEnd;
+
+
+	selfStart = this->pos + this.hitbox.topleft;
+	selfEnd   = this->pos + this.hitbox.botright;
+	compStart = other.getPos() + other.getHitbox().topleft;
+	compEnd   = other.getPos() + other.getHitbox().botRight;
+
+	vertAlign = max(selfStart.x, compStart.x) > min(selfEnd.x, compEnd.x);
+	vertAlign = max(selfStart.y, compStart.y) > min(selfEnd.y, compEnd.y);
+
+
+	return vertAlign && horzAlign;
+}
