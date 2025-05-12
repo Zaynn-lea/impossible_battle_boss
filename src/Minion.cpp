64946d5365180ef2221278f5a9914d14b29fdee3
Zaynn-lea <gely.leandre@gmail.com>
2025-05-12 11:38:36 +0200

    corrected misunderstanding about hitbox

--- src/Minion.cpp
+++ src/Minion.cpp
@@ -9,7 +9,7 @@ using namespace ImpossibleBattleBoss;
 cMinion::cMinion() : cEntity() {}
 
 cMinion::cMinion(olc::vi2d spawnPos, std::map<MinionState, std::vector<olc::Sprite *>> * sprites)
-	: cEntity(spawnPos, createHitbox(spawnPos.x, spawnPos.y, spawnPos.x + (*sprites)[ALIVE_MINION][0]->width, spawnPos.y + (*sprites)[ALIVE_MINION][0]->height), (*sprites)[ALIVE_MINION][0], MINION)
+	: cEntity(spawnPos, createHitbox(0, 0, (*sprites)[ALIVE_MINION][0]->width, (*sprites)[ALIVE_MINION][0]->height), (*sprites)[ALIVE_MINION][0], MINION)
 {
 	this->sprites	= sprites;
 	state 		= ALIVE_MINION;
--- src/PlayerClass.cpp
+++ src/PlayerClass.cpp
@@ -11,7 +11,7 @@ using namespace ImpossibleBattleBoss;
 cPlayer::cPlayer() : cEntity() {}
 
 cPlayer::cPlayer(olc::vi2d spawnCoords, std::map<PlayerState, std::vector<olc::Sprite *>> * sprites)
-	: cEntity(spawnCoords, createHitbox(spawnCoords.x, spawnCoords.y, spawnCoords.x + (*sprites)[IDLE_PLAYER][0]->width, spawnCoords.y + (*sprites)[IDLE_PLAYER][0]->height), (*sprites)[IDLE_PLAYER][0], PLAYER)
+	: cEntity(spawnCoords, createHitbox(0, 0, (*sprites)[IDLE_PLAYER][0]->width, (*sprites)[IDLE_PLAYER][0]->height), (*sprites)[IDLE_PLAYER][0], PLAYER)
 {
 	this->sprites = sprites;
 	state	      = IDLE_PLAYER;
